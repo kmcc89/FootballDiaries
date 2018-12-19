e43b8e8002ce4b97fd8bd16acae72dad87db6bc0
mnoone <mike.noone.mn@gmail.com>
2018-12-19 16:37:50 +0000

    Michael Noone 16.37 Links jinja 19.12.2018

--- webapp/FootballFlask.py
+++ webapp/FootballFlask.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 #from flask_nav import Nav
 #from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
-
 from DBcm import UseDatabase
 
 
@@ -13,6 +12,8 @@ app.config['dbconfig'] = {  'host':'172.20.10.5',
                             'password':'pass',
                             'database':'test_football',}
 
+
+
 @app.route('/')
 def hello() -> str:
     return 'Hello from football diary!'
@@ -20,6 +21,30 @@ def hello() -> str:
 @app.route('/about')
 def viewabout() -> 'html':
     return render_template('aboutpage.html',)
+
+@app.route('/login')
+def viewlogin() -> 'html':
+    return render_template('login.html',)
+
+@app.route('/statstable')
+def viewstatstable() -> 'html':
+    return render_template('statstable.html',)
+
+@app.route('/statsbar')
+def viewstatsbar() -> 'html':
+    return render_template('statsbar.html',)
+
+@app.route('/statsgraph')
+def viewstatsgraph() -> 'html':
+    return render_template('statsgraph.html',)
+
+@app.route('/statsQandA')
+def viewstatsQandA() -> 'html':
+    return render_template('statsQandA.html',)
+
+@app.route('/statsmatches')
+def viewstatsmatches() -> 'html':
+    return render_template('statsmatches.html',)
 
 @app.route('/viewResults')
 def show_book_reviews() -> 'html':
@@ -67,6 +92,8 @@ def show_united_wins() -> 'html':
 
     return render_template('cards.html',
                            the_title='Wins',
-                           the_row_titles=titles,
-                           the_data=contents, )
+                           my_name='Mike', )
 app.run(debug=True)
+
+
+
